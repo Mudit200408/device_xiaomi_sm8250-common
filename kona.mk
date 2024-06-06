d9f033aa13570a9f3934dceabb394f1f6180bf75
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-06-06 15:26:36 +0530

    sm8250-common: Add OP Dolby Support
    git clone https://github.com/Mudit200408/android_hardware_dolby hardware/dolby
    
    Co-Authored-By: Ivan Martynov <johnivan19999@gmail.com>
    Co-Authored-By: Omkar Chandorkar <forumomkar@gmail.com>
    Co-Authored-By: Pranav Vashi <1227398+neobuddy89@users.noreply.github.com>
    Co-Authored-By: someone5678 <59456192+someone5678@users.noreply.github.com>
    Co-Authored-By: Mudit200408 <muditpurohit2004.pm@gmail.com>

--- kona.mk
+++ kona.mk
@@ -10,6 +10,9 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 30 31 32 33 34
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+#Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
