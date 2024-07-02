729cb9d70decf8bc5e9c8cdd375aeb8c47afc79b
Mudit200408 <muditpurohit2004.pm@gmail.com>
2024-07-02 16:18:45 +0530

    sm8250-common: Adapt for Pixel-Project

--- kona.mk
+++ kona.mk
@@ -415,7 +415,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx_plugin.vendor
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
